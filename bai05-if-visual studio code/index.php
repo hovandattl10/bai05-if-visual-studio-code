055da70121a33c181e5e1dd5fab4debf9cbf2e3c
hovandattl10 <100410736+hovandattl10@users.noreply.github.com>
2022-02-25 16:30:22 +0700

    Add files via upload

---
+++ bai05-if-visual studio code/index.php
@@ -0,0 +1,15 @@
+<?php
+//giai phuong trinh bac nhat ax + b =0;
+$a = 5;
+$b = 6;
+if($a == 0){
+    if($b == 0){
+      echo 'Phuong trinh co vo so nghiem';
+}else{
+      echo 'Phuong trinh vo nghiem';
+    }
+}else{
+    $x = - $b / $a;
+    echo "pt co nghiem: $x";
+}      
+?>
